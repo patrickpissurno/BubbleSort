d79a90c4c800e99bd1163c77de15afc52c72f44d
Patrick Pissurno <contato@patrickpissurno.com.br>
2016-03-17 12:10:39 -0300

    Added graph and sorting proccess

--- BubbleSort/Form1.Designer.cs
+++ BubbleSort/Form1.Designer.cs
@@ -28,12 +28,47 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend3 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea3.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea3);
+            this.chart1.Dock = System.Windows.Forms.DockStyle.Fill;
+            legend3.Name = "Legend1";
+            this.chart1.Legends.Add(legend3);
+            this.chart1.Location = new System.Drawing.Point(0, 0);
+            this.chart1.Name = "chart1";
+            series3.ChartArea = "ChartArea1";
+            series3.Legend = "Legend1";
+            series3.Name = "Series1";
+            this.chart1.Series.Add(series3);
+            this.chart1.Size = new System.Drawing.Size(365, 331);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(365, 331);
+            this.Controls.Add(this.chart1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Bubble Sort";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
     }
 }
 
--- BubbleSort/Form1.cs
+++ BubbleSort/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -15,6 +16,49 @@ namespace BubbleSort
         public Form1()
         {
             InitializeComponent();
+            chart1.Series[0].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            chart1.Series[0].LegendText = "Temp(ms) x Quantidade";
+            GenerateGraph();
+        }
+
+        private int[] GenerateData(int amount)
+        {
+            int[] data = new int[amount];
+            Random r = new Random();
+            for (int i = 0; i < amount; i++)
+                data[i] = (r.Next(0, 4000));
+            return data;
+        }
+
+        private void GenerateGraph()
+        {
+            chart1.Series[0].Points.Clear();
+            for(int i = 10; i < 2000; i += 5)
+            {
+                int[] data = GenerateData(i);
+                var sw = new Stopwatch();
+                sw.Start();
+                data = BubbleSort(data);
+                sw.Stop();
+                chart1.Series[0].Points.AddXY(i, sw.Elapsed.TotalMilliseconds);
+            }
+        }
+
+        private int[] BubbleSort(int[] data)
+        {
+            for(int i=0; i < data.Length - 1; i++)
+            {
+                for (int j = i + 1; j < data.Length; j++)
+                {
+                    if(data[i] > data[j])
+                    {
+                        int temp = data[i];
+                        data[i] = data[j];
+                        data[j] = temp;
+                    }
+                }
+            }
+            return data;
         }
     }
 }
